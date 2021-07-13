142a461a6b8ba50d517a082e077f62704ca6aee6
Yanis B <zenaehyle@protonmail.com>
2021-07-13 12:04:59 +0200

    Ajout de la configuration pour VSCode

--- packages/api/src/index.js
+++ packages/api/src/index.js
@@ -1,12 +1,11 @@
 const express = require("express");
 const app = express();
 
+// eslint-disable-next-line no-unused-vars
 app.get("/", (req, res) => {
-  console.log("ok");
+  console.log("Api");
 });
 
 app.listen(8080, () => {
   console.log("server started");
 });
-
-console.log("hello");
